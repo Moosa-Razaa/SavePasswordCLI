04d63a98deb94131b8104c408e751e3dbcdc1d71
Moosa-Razaa <moosaraza1k99@gmail.com>
2025-04-08 14:12:18 +0200

    reinstantiate the project

--- Application/Interfaces/ActionHandler.cs
+++
@@ -1,6 +0,0 @@
-namespace Application.Interfaces;
-
-public interface IActionHandler
-{
-    void HandleAction();
-}
--- Application/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-var os = Environment.OSVersion;
-Console.WriteLine("Platform : " + os.Platform);
